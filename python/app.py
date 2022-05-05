cd9113496beef8ce98fc3c751a75a977179e8bfd
Juliette <firebuggirl@users.noreply.github.com>
2022-05-05 12:24:56 -0700

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello Desert Peoples!"
+    return "Hello Desert Peoples! It's the month of May and it's beginning to get a bit toasty outside!!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
